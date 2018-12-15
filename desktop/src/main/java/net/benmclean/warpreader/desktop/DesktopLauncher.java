f393902bd9b66d08e74c5e06d80000c0758ca67a
Benjamin McLean <mclean.ben@gmail.com>
2018-12-15 07:50:36 -0600

    Got it running

--- desktop/src/main/java/net/benmclean/warpreader/desktop/DesktopLauncher.java
+++ desktop/src/main/java/net/benmclean/warpreader/desktop/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package net.benmclean.warpreader.desktop;
 
-import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import net.benmclean.warpreader.WarpReader;
@@ -20,9 +19,9 @@ public class DesktopLauncher {
         configuration.title = "WarpReader";
         configuration.width = 640;
         configuration.height = 480;
-        for (int size : new int[] { 128, 64, 32, 16 }) {
-            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
-        }
+//        for (int size : new int[] { 128, 64, 32, 16 }) {
+//            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+//        }
         return configuration;
     }
 }
