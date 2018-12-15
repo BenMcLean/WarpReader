3f76ef438d1aa2841aaccf7ef52a32fb147c396d
Benjamin McLean <mclean.ben@gmail.com>
2018-12-15 08:25:50 -0600

    Copied code over from SimpleTest

--- core/src/main/java/net/benmclean/warpreader/WarpReader.java
+++ core/src/main/java/net/benmclean/warpreader/WarpReader.java
@@ -2,24 +2,204 @@ package net.benmclean.warpreader;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import squidpony.StringKit;
+import warpwriter.Coloring;
+import warpwriter.ModelMaker;
+import warpwriter.model.IModel;
+import warpwriter.model.fetch.ArrayModel;
+import warpwriter.model.fetch.BoxModel;
+import warpwriter.model.fetch.ColorFetch;
+import warpwriter.view.Twilight;
 import warpwriter.view.VoxelSprite;
+import warpwriter.view.VoxelSpriteBatchRenderer;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. Listens to user input.
+ */
 public class WarpReader extends InputAdapter implements ApplicationListener {
+    /**
+     * This is the default vertex shader from libGDX.
+     */
+    public static final String vertexShader = "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+            + "attribute vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+            + "attribute vec2 " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+            + "uniform mat4 u_projTrans;\n" //
+            + "varying vec4 v_color;\n" //
+            + "varying vec2 v_texCoords;\n" //
+            + "\n" //
+            + "void main()\n" //
+            + "{\n" //
+            + "   v_color = " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
+            + "   v_color.a = v_color.a * (255.0/254.0);\n" //
+            + "   v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
+            + "   gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+            + "}\n";
+
+    /**
+     * This fragment shader draws a black outline around things.
+     */
+    public static final String fragmentShader = "#version 150\n" +
+            "varying vec2 v_texCoords;\n" +
+            "varying vec4 v_color;\n" +
+            "uniform float outlineH;\n" +
+            "uniform float outlineW;\n" +
+            "uniform sampler2D u_texture;\n" +
+            "void main()\n" +
+            "{\n" +
+            "   vec2 offsetx;\n" +
+            "   offsetx.x = outlineW;\n" +
+            "   vec2 offsety;\n" +
+            "   offsety.y = outlineH;\n" +
+            "   float alpha = texture2D( u_texture, v_texCoords ).a;\n" +
+            "   alpha = max(alpha, texture2D( u_texture, v_texCoords + offsetx).a);\n" +
+            "   alpha = max(alpha, texture2D( u_texture, v_texCoords - offsetx).a);\n" +
+            "   alpha = max(alpha, texture2D( u_texture, v_texCoords + offsety).a);\n" +
+            "   alpha = max(alpha, texture2D( u_texture, v_texCoords - offsety).a);\n" +
+            "   gl_FragColor = v_color * texture2D( u_texture, v_texCoords );\n" +
+            "   gl_FragColor.a = alpha;\n" +
+            "}";
+
+    public static final int backgroundColor = Color.rgba8888(Color.DARK_GRAY);
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+    public static final int VIRTUAL_WIDTH = 640;
+    public static final int VIRTUAL_HEIGHT = 360;
+    protected SpriteBatch batch;
+    protected Viewport worldView;
+    protected Viewport screenView;
+    protected BitmapFont font;
+    protected FrameBuffer buffer;
+    protected Texture screenTexture;
+    protected TextureRegion screenRegion;
+    protected ModelMaker maker;
     protected VoxelSprite voxelSprite;
+    protected boolean box = false;
+    protected VoxelSpriteBatchRenderer batchRenderer;
+    protected ShaderProgram shader;
+    protected ShaderProgram defaultShader;
 
     @Override
     public void create() {
+        font = new BitmapFont(Gdx.files.internal("PxPlus_IBM_VGA_8x16.fnt"));
+        batch = new SpriteBatch();
+        worldView = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+        screenView = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+        buffer = new FrameBuffer(Pixmap.Format.RGBA8888, VIRTUAL_WIDTH, VIRTUAL_HEIGHT, false, false);
+        screenRegion = new TextureRegion();
+        screenView.getCamera().position.set(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 0);
+        screenView.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.enableBlending();
+
+        maker = new ModelMaker(12345);
+        batchRenderer = new VoxelSpriteBatchRenderer(batch);
+//        batchRenderer.set(batchRenderer.color().set(VoxelColor.AuroraTwilight)); // comment out to use Rinsed
+        voxelSprite = new VoxelSprite()
+                .set(batchRenderer)
+                .setOffset(VIRTUAL_WIDTH / 2, 100);
+        makeModel();
+
+        defaultShader = SpriteBatch.createDefaultShader();
+        shader = new ShaderProgram(vertexShader, fragmentShader);
+        if (!shader.isCompiled()) throw new GdxRuntimeException("Couldn't compile shader: " + shader.getLog());
+
         Gdx.input.setInputProcessor(this);
+    }
+
+    public void makeModel() {
+        voxelSprite.set(
+                box ?
+                        new BoxModel(model(), ColorFetch.color(Coloring.rinsed("Powder Blue 3")))
+                        : model()
+        );
+    }
+
+    public IModel model() {
+//        return new VoxelText()
+//                .setText(font, FakeLanguageGen.SIMPLISH.word(maker.rng.nextLong(), true))
+//                .setFill(ColorFetch.color(Coloring.rinsed("Powder Blue 3")))
+//                .setOutline(ColorFetch.color(Coloring.rinsed("Red 4")))
+//                .setDepth(5);
+        try {
+            //// loads Artillery.vox, which has a palette set in that model; this palette doesn't match Rinsed or Aurora
+//            final byte[][][] arr = VoxIO.readVox(new LittleEndianDataInputStream(new FileInputStream("HasOwnPalette/Artillery.vox")));
+            //// set the palette to the one from the vox model, using arbitraryTwilight()
+//            batchRenderer.set(batchRenderer.color().set(Twilight.arbitraryTwilight(VoxIO.lastPalette)));
+            batchRenderer.color().set(Twilight.arbitraryTwilight(Coloring.AURORA));
+            return new ArrayModel(
+                    maker.shipLargeRandomAurora()
+//                    arr
+                    //// Aurora folder has vox models with a different palette, which involves a different ITwilight.
+                    //VoxIO.readVox(new LittleEndianDataInputStream(new FileInputStream("Aurora/Warrior_Male_W.vox")))
+                    // If using Rinsed, use the line below instead of the one above.
+                    //maker.warriorRandom()
+            );
+        } catch (Exception e) {
+            return new ArrayModel(maker.warriorRandom());
+        }
     }
 
     @Override
     public void resize(final int width, final int height) {
+        screenView.update(width, height);
     }
 
     @Override
     public void render() {
+        buffer.begin();
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        worldView.apply();
+        worldView.getCamera().position.set(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 0);
+        worldView.update(VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+        batch.setProjectionMatrix(worldView.getCamera().combined);
+        batch.begin();
+
+        font.draw(batch, StringKit.join(", ", voxelSprite.getModel().sizeX(), voxelSprite.getModel().sizeY(), voxelSprite.getModel().sizeZ()) + " (original)", 0, 80);
+        font.draw(batch, voxelSprite.turnModel().sizeX() + ", " + voxelSprite.turnModel().sizeY() + ", " + voxelSprite.turnModel().sizeZ() + " (modified)", 0, 60);
+        font.draw(batch, StringKit.join(", ", voxelSprite.turnModel().turner().rotation()) + " (rotation)", 0, 40);
+        font.draw(batch, Gdx.graphics.getFramesPerSecond() + " FPS", 0, 20);
+
+        voxelSprite.render();
+
+        batch.setColor(-0x1.fffffep126f); // white as a packed float, resets any color changes that the renderer made
+        batch.end();
+        buffer.end();
+        Gdx.gl.glClearColor(
+                ((backgroundColor >> 24) & 0xff) / 255f,
+                ((backgroundColor >> 16) & 0xff) / 255f,
+                ((backgroundColor >> 8) & 0xff) / 255f,
+                (backgroundColor & 0xff) / 255f
+        );
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        screenView.apply();
+        batch.setProjectionMatrix(screenView.getCamera().combined);
+        batch.begin();
+        screenTexture = buffer.getColorBufferTexture();
+        screenTexture.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+        screenRegion.setRegion(screenTexture);
+        screenRegion.flip(false, true);
+
+        batch.setShader(shader);
+        shader.setUniformf("outlineH", 1f / VIRTUAL_HEIGHT);
+        shader.setUniformf("outlineW", 1f / VIRTUAL_WIDTH);
+
+        batch.draw(screenRegion, 0, 0);
+        batch.setShader(defaultShader);
+        batch.end();
     }
 
     @Override
@@ -34,5 +214,59 @@ public class WarpReader extends InputAdapter implements ApplicationListener {
     public void dispose() {
     }
 
-    // Note: you can override methods from InputAdapter API to handle user's input.
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.NUM_0:
+                voxelSprite.setAngle(1);
+                break;
+            case Input.Keys.MINUS:
+                voxelSprite.setAngle(2);
+                break;
+            case Input.Keys.EQUALS:
+                voxelSprite.setAngle(3);
+                break;
+            case Input.Keys.U:
+                voxelSprite.clockX();
+                break;
+            case Input.Keys.I:
+                voxelSprite.clockY();
+                break;
+            case Input.Keys.O:
+                voxelSprite.clockZ();
+                break;
+            case Input.Keys.J:
+                voxelSprite.counterX();
+                break;
+            case Input.Keys.K:
+                voxelSprite.counterY();
+                break;
+            case Input.Keys.L:
+                voxelSprite.counterZ();
+                break;
+            case Input.Keys.R:
+                voxelSprite.reset();
+                break;
+            case Input.Keys.P:
+                makeModel();
+                break;
+            case Input.Keys.B:
+                box = !box;
+                makeModel();
+                break;
+            case Input.Keys.G:
+                batchRenderer.color().set(batchRenderer.color().direction().counter());
+                break;
+            case Input.Keys.H:
+                batchRenderer.color().set(batchRenderer.color().direction().clock());
+                break;
+            case Input.Keys.T: // try again
+                voxelSprite.reset();
+                break;
+            case Input.Keys.ESCAPE:
+                Gdx.app.exit();
+                break;
+        }
+        return true;
+    }
 }
