e81f171393d3fadaab77b5432154f55d9990b2b5
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-02-19 21:01:19 -0800

    Switch to LWJGL3, add some start of code to maybe deal with HDPI

--- desktop/src/main/java/net/benmclean/warpreader/desktop/DesktopLauncher.java
+++ desktop/src/main/java/net/benmclean/warpreader/desktop/DesktopLauncher.java
@@ -1,27 +1,27 @@
 package net.benmclean.warpreader.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import net.benmclean.warpreader.WarpReader;
 
-/** Launches the desktop (LWJGL) application. */
+/** Launches the desktop (LWJGL3) application. */
 public class DesktopLauncher {
     public static void main(String[] args) {
         createApplication();
     }
 
-    private static LwjglApplication createApplication() {
-        return new LwjglApplication(new WarpReader(), getDefaultConfiguration());
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new WarpReader(), getDefaultConfiguration());
     }
 
-    private static LwjglApplicationConfiguration getDefaultConfiguration() {
-        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
-        configuration.title = "WarpReader";
-        configuration.width = 640;
-        configuration.height = 480;
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("WarpReader");
+        config.setWindowedMode(640, 480);
+        config.setHdpiMode(Lwjgl3ApplicationConfiguration.HdpiMode.Pixels);
 //        for (int size : new int[] { 128, 64, 32, 16 }) {
 //            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
 //        }
-        return configuration;
+        return config;
     }
 }
